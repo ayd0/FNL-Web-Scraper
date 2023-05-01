3fdc0c56620159ae99cee5d6c5c5473c39a75b49
ayd0 <73216273+ayd0@users.noreply.github.com>
2023-04-30 20:58:05 -0700

    branch loc verification, set express indicator, update element evaluation

--- fnl_scraper.js
+++ fnl_scraper.js
@@ -1,8 +1,13 @@
-// TODO: POST to location field to narrow store locations
+// TODO: 
+//       - convert fnl string to search string
+//       - convert results to XML format and POST to
+//       - certify location updated
+
 const puppeteer = require("puppeteer");
 
-const PART_DESCRIPTION = (fnl, boundry) =>
-    `https://fastenal.com/product?query=${fnl}`;
+const MAIN_PAGE = "https://fastenal.com/";
+const LOC_PAGE = `${MAIN_PAGE}/locations/details/WAFER`;
+const PART_DESCRIPTION = (fnl) => `${MAIN_PAGE}product?query=${fnl}`;
 
 const self = {
     browser: null,
@@ -12,38 +17,55 @@ const self = {
         self.browser = await puppeteer.launch({
             headless: false,
         });
-        self.page = await self.browser.newPage();
+        await self.postLoc();
 
         await self.page.goto(PART_DESCRIPTION(fnl), {
             waitUntil: "networkidle0",
         });
+        await self.setExpressIndicator();
     },
-    getResults: async (boundry=null) => {
-        let elements = await self.page.$$(".ecom-card-sm");
+    postLoc: async () => {
+        self.page = await self.browser.newPage();
+        await self.page.goto(LOC_PAGE, {
+            waitUntil: "networkidle0",
+        });
+        await self.page.click(".js-setStore", {
+            waitUntil: "networkidle0",
+        });
+    },
+    setExpressIndicator: async () => {
+        await self.page.click(".form-check-input", {
+            waitUntil: "networkidle0",
+        });
+    },
+    getResults: async (boundry = null) => {
+        let elements; // = await self.page.$$(".ecom-card-sm");
         let results = [];
+        await self.page
+            .waitForSelector(".ecom-card-icon-list-group")
+            .then(console.log("selecting..."))
+            .then((elements = await self.page.$$(".ecom-card-sm")))
+            .then(console.log(`selected: ${elements}`))
+            .then(async () => {
+                if (boundry)
+                    boundry =
+                        boundry.toUpperCase()[0] +
+                        boundry.slice(1).toLowerCase();
 
-        if (boundry)
-            boundry = boundry.toUpperCase()[0] + boundry.slice(1).toLowerCase();
+                for (let element of elements) {
+                    let partNum = await element.$eval(".font-weight-normal", (node) => node.innerText.trim());
+                    let partDesc = await element.$eval(".card-text > span", (node) => node.innerText.trim());
+                    console.log(`PATH: ${typeof(partNum)}: ${partNum.length}`);
+                    let list = {
+                        partNum,
+                        partDesc
+                    };
 
-        for (let element of elements) {
-            let partPath = await element.$eval("div", (node) =>
-                node.innerText.trim()
-            );
-            let partNum = `${partPath}`.split("\n")[1].slice(25);
-            let partDesc = `${partPath}`.split("\n")[3];
-            let list = {
-                partNum,
-                partDesc,
-            };
+                    results.push(list);
+                }
+            });
 
-            if (boundry) {
-                if (partDesc.includes(boundry)) results.push(list);
-                continue;
-            }
-
-            results.push(list);
-        }
-
+        // await self.browser.close();
         return results;
     },
     processBoundry: async (partDesc, boundry) => {},
